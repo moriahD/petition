1cfec87710d5b4e64e70d336375774226ecd22ac
moriahdaimmo <moriahdaimmo@gmail.com>
2019-07-06 16:24:44 +0200

    replaced canvas instead of input for signature

--- public/script.js
+++ public/script.js
@@ -1,23 +1,34 @@
 var signature = $("#signature");
 var wrap_signature = $("#wrap_signature");
+// var dataURL = signature.toDataURL();
+var c = signature.getContext("2d");
+
 var mousedown = false;
-
-signature.on("mousedown", function(e) {
-    e.preventDefault();
-    mousedown = true;
-    console.log("mouse down");
-});
-
-signature.on("mouseup", function(e) {
-    e.preventDefault();
-    mousedown = false;
-    console.log("mouse down");
-});
+signature
+    .on("mousedown", function(e) {
+        e.preventDefault();
+        mousedown = true;
+        console.log("mouse down");
+    })
+    .on("touchstart", function() {
+        mousedown = true;
+    });
+signature
+    .on("mouseup", function(e) {
+        e.preventDefault();
+        mousedown = false;
+        console.log("mouse down");
+    })
+    .on("touchend", function() {
+        mousedown = false;
+    });
 
 signature.on("mousemove", function() {
     if (mousedown) {
         console.log("mouse writing");
+        c.strokeStyle = "black";
+        c.lineWidth = "2";
+        c.beginPath();
     } else {
-        console.log("mouse moving without mousedown");
     }
 });
