c1d9cfada99a20bdf3ae11703a40b2c517f015ed
moriahdaimmo <moriahdaimmo@gmail.com>
2019-07-06 17:16:04 +0200

    i can draw signature but with weird position

--- public/script.js
+++ public/script.js
@@ -1,13 +1,24 @@
-var signature = $("#signature");
-var wrap_signature = $("#wrap_signature");
+//#signature is id of canvas
 // var dataURL = signature.toDataURL();
-var c = signature.getContext("2d");
-
+var signature = $("#signature");
 var mousedown = false;
+var pos = { x: 0, y: 0 };
+function setPosition(e) {
+    pos.x = e.clientX;
+    pos.y = e.clientY;
+}
+var signatureElement = document.getElementById("signature");
+var c = signatureElement.getContext("2d");
+
+console.log(c);
+c.strokeStyle = "black";
+c.lineWidth = "2";
+
 signature
     .on("mousedown", function(e) {
         e.preventDefault();
         mousedown = true;
+        setPosition(e);
         console.log("mouse down");
     })
     .on("touchstart", function() {
@@ -17,18 +28,22 @@ signature
     .on("mouseup", function(e) {
         e.preventDefault();
         mousedown = false;
-        console.log("mouse down");
+        console.log("mouse up");
     })
     .on("touchend", function() {
         mousedown = false;
     });
 
-signature.on("mousemove", function() {
+signature.on("mousemove", function(e) {
     if (mousedown) {
         console.log("mouse writing");
-        c.strokeStyle = "black";
-        c.lineWidth = "2";
+
         c.beginPath();
+        c.moveTo(pos.x, pos.y); // from
+        setPosition(e);
+        c.lineTo(pos.x, pos.y);
+
+        c.stroke();
     } else {
     }
 });
